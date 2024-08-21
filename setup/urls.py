f578a42fbed74eed5c3d85e3ae23afea322f6e16
ranierelp <raierepereira21@outlook.com>
2024-08-21 11:28:04 -0300

    Matricula, ListaMatriculasEstudanteSerializer, ListaEstudantesMatriculaSerializer

--- setup/urls.py
+++ setup/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from escola.views import EstudanteViewSet, CursoViewSet
+from escola.views import EstudanteViewSet, CursoViewSet, MatriculaViewSet
 
 router = routers.DefaultRouter()
 router.register('estudantes', EstudanteViewSet, basename='Estudante')
 router.register('cursos', CursoViewSet, basename='Curso')
+router.register('matriculas', MatriculaViewSet, basename='Matriculas')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
